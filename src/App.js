8eefe51df6448ca2012cd79733a97e97a3aea7d3
jacksonfernandolima <148342108+jacksonfernandolima@users.noreply.github.com>
2023-11-09 20:40:06 +0000

    routes

--- src/App.js
+++ src/App.js
@@ -1,48 +1,10 @@
-import Banner from "./components/Banner";
-import Card from "./components/Card";
-import Category from "./components/Category";
-import Container from "./components/Container";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import videos from "./json/videos.json";
 
-const categories = [
-  "Lançamentos 2023",
-  "Video Aula - Boss GT 100",
-  "Antigos"
-]
-
-function filterCategory(id) {
-  return videos.filter( video => video.category === categories[id])
-}
+import AppRoutes from "./Routes";
 
 function App() {
   return (
 
-    <>
-     <Header/>
-     <Banner image="home" />
-     <Container>
-     {/* <h2>Lançamentos 2023</h2>
-      <section className="cards">
-     {videos.map((videos) => {return <Card id={videos.id} key={videos.id} />})}
-     </section>*/}
-
-     <Category category={categories[0]}>
-     {filterCategory(0).map((videos) => {return <Card id={videos.id} key={videos.id} />})}
-     </Category>
-
-     <Category category={categories[1]}>
-     {filterCategory(1).map((videos) => {return <Card id={videos.id} key={videos.id} />})}
-     </Category>
-     <Category category={categories[2]}>
-     {filterCategory(2).map((videos) => {return <Card id={videos.id} key={videos.id} />})}
-     </Category>
-
-    </Container>
-    <Footer />
-   </>
-  
+   <AppRoutes />
   );
 }
 
--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,4 +1,15 @@
+import videos from "../../json/videos.json";
 import styles from "./Category.module.css";
+
+export const categories = [
+    "Lançamentos 2023",
+    "Video Aula - Boss GT 100",
+    "Antigos"
+  ]
+  
+ export function filterCategory(id) {
+    return videos.filter( video => video.category === categories[id])
+  }
 
 function Category({ category,children }) {
     return (
