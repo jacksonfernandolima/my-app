db90301c627461b1a3f5b2c3823ee73dd652a63c
jacksonfernandolima <148342108+jacksonfernandolima@users.noreply.github.com>
2023-11-09 11:48:31 +0000

    Projeto netflix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import Banner from "./components/Banner";
+import Card from "./components/Card";
+import Category from "./components/Category";
+import Container from "./components/Container";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import videos from "./json/videos.json";
+
+const categories = [
+  "Lançamentos 2023",
+  "Video Aula - Boss GT 100",
+  "Antigos"
+]
+
+function filterCategory(id) {
+  return videos.filter( video => video.category === categories[id])
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+     <Header/>
+     <Banner image="home" />
+     <Container>
+     {/* <h2>Lançamentos 2023</h2>
+      <section className="cards">
+     {videos.map((videos) => {return <Card id={videos.id} key={videos.id} />})}
+     </section>*/}
+
+     <Category category={categories[0]}>
+     {filterCategory(0).map((videos) => {return <Card id={videos.id} key={videos.id} />})}
+     </Category>
+
+     <Category category={categories[1]}>
+     {filterCategory(1).map((videos) => {return <Card id={videos.id} key={videos.id} />})}
+     </Category>
+     <Category category={categories[2]}>
+     {filterCategory(2).map((videos) => {return <Card id={videos.id} key={videos.id} />})}
+     </Category>
+
+    </Container>
+    <Footer />
+   </>
+  
   );
 }
 
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+    return (
+        <footer className={styles.footer}> 
+            <h2>Jackflix &copy; Desenvolvido por Jackson Fernando 2023</h2>
+        </footer>
+        );
+
+}
+export default Footer;
