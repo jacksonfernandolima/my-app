f3afa1479a6dccc83e9228a71894f41c959fe313
jacksonfernandolima <148342108+jacksonfernandolima@users.noreply.github.com>
2023-11-13 13:16:44 +0000

    Favoritos adicionados !

--- src/Routes.js
+++ src/Routes.js
@@ -3,17 +3,21 @@ import Home from "./Pages/Home";
 import Watch from "./Pages/Watch";
 import PageNotFound from "./Pages/PageNotFound";
 import Search from "./Pages/Search";
+import Favorites from "./Pages/Favorites";
+import FavoritesProvider from "./contexts/Favorites";
 
 function AppRoutes() {
-    return(
+    return (
         <BrowserRouter>
-         <Routes>
-            <Route path="/" element={ <Home /> }></Route>
-            <Route path="/Watch/:id" element={ <Watch /> }></Route>
-            <Route path="/search" element={ <Search /> }></Route>
-            <Route path="*" element={ <PageNotFound /> }></Route>
-
-         </Routes>
+            <FavoritesProvider>
+                <Routes>
+                    <Route path="/" element={ <Home /> }></Route>
+                    <Route path="/Watch/:id" element={ <Watch /> }></Route>
+                    <Route path="/search" element={ <Search /> }></Route>
+                    <Route path="/favorites" element={ <Favorites /> }></Route>
+                    <Route path="*" element={ <PageNotFound /> }></Route>
+                </Routes>
+            </FavoritesProvider>
         </BrowserRouter>
     );
 }
